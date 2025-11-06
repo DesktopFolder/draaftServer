6f840e3304640c1755f2200366c0bb10bb6020b0
DesktopFolder <desktopfolder@users.noreply.github.com>
2025-11-05 19:02:07 -0500

    /draft: Slightly more drafting work

--- src/draft.py
+++ src/draft.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Literal
 from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException, Request, Response
@@ -6,9 +7,19 @@ from utils import LOG
 
 rt = APIRouter(prefix='/draft')
 
+class AutoName(BaseModel):
+    @staticmethod
+    def make_simple(name: str) -> 'AutoName':
+        return AutoName(full_name=name, short_name=name)
+
+    full_name: str
+    short_name: str
+
 class Draftable(BaseModel):
     # How other objects refer to this object
     key: str
+
+    name: AutoName
 
     # Generally, the pop-up text for this object
     description: str
@@ -16,12 +27,39 @@ class Draftable(BaseModel):
     # The image tied to this object
     image_uri: str
 
+    @staticmethod
+    def basic(key: str, description: str, image: str | None = None, name: str | None = None) -> "Draftable":
+        if image is None:
+            image = f'{key}.png'
+        if name is None:
+            name = key
+        return Draftable(key=key, name=AutoName.make_simple(name), description=description, image_uri=image)
+
+class PoolTypeEnum(str, Enum):
+    icons = 'icons'
+    auto_names = 'auto_names'
+
+class DraftPool(BaseModel):
+    name: AutoName
+
+    # Draftables (keys)
+    contains: list[str]
+
+    # Pool type
+    kind: PoolTypeEnum
+
 
 DRAFTABLES: dict[str, Draftable] = {}
 _draftable_file = "draftables.json"
 def _add_draftable(d: Draftable):
     DRAFTABLES[d.key] = d
-_add_draftable(Draftable(key="bucket", description="A fully-enchanted, max-tier bucket.", image_uri="bucket.gif"))
+_add_draftable(Draftable.basic(key="bucket", description="A fully-enchanted, max-tier bucket.", image="bucket.gif"))
+
+
+# Configurable later, just get it working for now.
+POOLS: list[DraftPool] = [
+    DraftPool(name=AutoName.make_simple('Miscellaneous'), contains=["bucket"], kind=PoolTypeEnum.auto_names)
+]
 
 
 class DraftPick(BaseModel):
@@ -55,6 +93,7 @@ class Draft(BaseModel):
     players: list[str] = list()
     draft: list[DraftPick] = list()
     position: list[str] = list()
+    picked: set[str] = set()
 
 @rt.get('/status')
 async def get_status(request: Request) -> Draft:
@@ -66,8 +105,8 @@ async def get_status(request: Request) -> Draft:
     return ru[2]
 
 @rt.get('/draftables')
-async def get_draftables() -> dict[str, Draftable]:
-    return DRAFTABLES
+async def get_draftables() -> tuple[list[DraftPool], dict[str, Draftable]]:
+    return (POOLS, DRAFTABLES)
 
 @rt.post('/pick')
 async def do_pick(request: Request, key: str):
@@ -80,6 +119,8 @@ async def do_pick(request: Request, key: str):
         raise HTTPException(status_code=403, detail="You cannot pick right now.")
     if key not in DRAFTABLES:
         raise HTTPException(status_code=404, detail=f"Draft pick {key} could not be found.")
+    if key in draft.picked:
+        raise HTTPException(status_code=403, detail=f"The key {key} has already been picked.")
 
     p = DraftPick(key=key, player=user.uuid, index=len(draft.draft))
 
@@ -96,6 +137,8 @@ async def do_pick(request: Request, key: str):
         else:
             draft.position = list(draft.players)
 
+    draft.picked.add(key)
     if not update_draft(draft, room.code):
         raise HTTPException(status_code=500, detail='Could not update draft internally..!')
+
     await mg.broadcast_room(room, p)
