9cd554faacd21c915f0b2a57e03733d11b4a39c6
DesktopFolder <desktopfolder@users.noreply.github.com>
2026-02-19 21:17:17 -0500

    backup better

--- backup.sh
+++ backup.sh
@@ -7,5 +7,14 @@ echo "Backing up: draaft.db"
 sqlite3 db/draaft.db ".backup 'backup.db'"
 mv backup.db .backup
 
+echo "Backing up: dotfiles"
+cp .visitors.json .backup
+cp .bracket-log.json .backup
+
+echo "Backing up: seeds"
+cp $HOME/data/draaft/generated_overworld_seeds.txt .backup
+cp $HOME/data/draaft/overworld_seeds_strongholds.txt .backup
+cp $HOME/data/draaft/overworld_seeds.txt .backup
+
 tar -czf backup.tar.gz .backup
 echo "Finished. To export, run: scp $(whoami)@$(hostname):$(pwd)/backup.tar.gz ."
